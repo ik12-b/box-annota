684eca84050a2a9b88272020eaa3e7292fdf44af
ik12-b <mmu49026@gmail.com>
2026-09-18 09:49:38 +0700

    fix some bug

--- app/src/main/java/com/example/ui/components/AnnotationCanvasView.kt
+++ app/src/main/java/com/example/ui/components/AnnotationCanvasView.kt
@@ -3,6 +3,7 @@ package com.example.ui.components
 import android.graphics.Bitmap
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
